d04da2134c6973c9cc0210ce2dbeb7c7714fa405
xz <471340101@qq.com>
2023-02-22 19:48:31 +0800

    feat: 增加了退出登录的功能

--- web/src/compoent/Header/Header.js
+++ web/src/compoent/Header/Header.js
@@ -1,24 +1,32 @@
-import React, {Component} from 'react';
-import { Layout } from 'antd';
+import React, { Component } from 'react';
+import { Button, Layout } from 'antd';
+import { useNavigate } from 'react-router-dom';
 // const { Header} = Layout;
-const  AntdHeader = Layout.Header;
+const AntdHeader = Layout.Header;
 
-
-class Header extends Component {
-    render() {
-        return (
-            <AntdHeader
-                className="site-layout-background"
-
-                style={{
-                    // position: 'sticky',
-                    top: 0, zIndex: 1,
-                    padding: 0,
-                    backgroundColor: "white"
-                }}
-            ></AntdHeader>
-        );
-    }
+function Header() {
+  let navigate = useNavigate();
+  const click = () => {
+    console.log('click');
+    localStorage.removeItem('token');
+    navigate('/login');
+  };
+  return (
+    <AntdHeader
+      className='site-layout-background'
+      style={{
+        // position: 'sticky',
+        top: 0,
+        zIndex: 1,
+        padding: 0,
+        backgroundColor: 'white',
+      }}
+    >
+      <Button style={{ marginLeft: 20 }} onClick={click}>
+        退出登录
+      </Button>
+    </AntdHeader>
+  );
 }
 
 export default Header;
